3722d509f753cd2659ad61c52babe31c6bc99e8d
Bilshansakib <burakaghani@gmail.com>
2024-06-02 03:19:24 +0600

    All camp cards added

---
+++ src/components/Shared/CampCard/AllAvailableCard.jsx
@@ -0,0 +1,61 @@
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+const { Meta } = Card;
+
+const AllAvailableCard = ({ card }) => {
+  const {
+    CampName,
+    Image,
+    CampFees,
+    DateTime,
+    Location,
+    HealthcareProfessional,
+    ParticipantCount,
+    Description,
+  } = card;
+  return (
+    <>
+      <div className="mx-auto gap-4">
+        <Card
+          style={{
+            width: 360,
+            marginTop: 16,
+          }}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+            }
+            title={CampName}
+            description={Description}
+          />
+        </Card>
+        <Card
+          style={{
+            width: 360,
+            marginTop: 2,
+          }}
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=2" />
+            }
+            title="Card title"
+            description="This is the description"
+          />
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default AllAvailableCard;
--- src/pages/AvailableCamps/AvailableCamps.jsx
+++ src/pages/AvailableCamps/AvailableCamps.jsx
@@ -1,17 +1,129 @@
 import { Helmet } from "react-helmet-async";
 import CampsCover from "../../components/Shared/Cover/CampsCover";
 import useCamp from "../../hooks/useCamp";
+import AllAvailableCard from "../../components/Shared/CampCard/AllAvailableCard";
+import { Avatar, Skeleton } from "antd";
+import Meta from "antd/es/card/Meta";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import {
+  TfiLayoutColumn2,
+  TfiLayoutColumn3,
+  TfiLayoutGrid2Alt,
+  TfiLayoutGrid3Alt,
+} from "react-icons/tfi";
+import { IconButton } from "@material-tailwind/react";
 
 const AvailableCamps = () => {
-  const [camp] = useCamp();
+  const [camp, loading] = useCamp();
   return (
-    <>
+    <div className="container mx-auto">
       <Helmet>
         <title>Medi+ | Available Camps</title>
       </Helmet>
       <CampsCover></CampsCover>
       <h2>{camp.length}</h2>
-    </>
+      <Tabs>
+        <div className="flex justify-end">
+          <Tab>
+            <IconButton variant="gradient">
+              <TfiLayoutGrid3Alt />
+            </IconButton>
+          </Tab>
+          <Tab>
+            <IconButton variant="outlined">
+              <TfiLayoutGrid2Alt />
+            </IconButton>
+          </Tab>
+        </div>
+
+        <TabPanel>
+          {loading ? (
+            <>
+              <div className="justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {camp.map((c) => (
+                  <Skeleton key={c._id} avatar active>
+                    <Meta
+                      avatar={
+                        <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=2" />
+                      }
+                      title="Card title"
+                      description="This is the description"
+                    />
+                  </Skeleton>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {camp.map((card) => (
+                  <AllAvailableCard
+                    key={card._id}
+                    card={card}
+                  ></AllAvailableCard>
+                ))}
+              </div>
+            </>
+          )}
+        </TabPanel>
+        <TabPanel>
+          {loading ? (
+            <>
+              <div className="justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {camp.map((c) => (
+                  <Skeleton key={c._id} avatar active>
+                    <Meta
+                      avatar={
+                        <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=2" />
+                      }
+                      title="Card title"
+                      description="This is the description"
+                    />
+                  </Skeleton>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                {camp.map((card) => (
+                  <AllAvailableCard
+                    key={card._id}
+                    card={card}
+                  ></AllAvailableCard>
+                ))}
+              </div>
+            </>
+          )}
+        </TabPanel>
+      </Tabs>
+      {/* {loading ? (
+        <>
+          <div className="justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {camp.map((c) => (
+              <Skeleton key={c._id} avatar active>
+                <Meta
+                  avatar={
+                    <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=2" />
+                  }
+                  title="Card title"
+                  description="This is the description"
+                />
+              </Skeleton>
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {camp.map((card) => (
+              <AllAvailableCard key={card._id} card={card}></AllAvailableCard>
+            ))}
+          </div>
+        </>
+      )} */}
+    </div>
   );
 };
 
